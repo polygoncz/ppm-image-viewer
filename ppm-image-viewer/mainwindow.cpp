89265459dac1d228f6fd67954fc4f6d5255f1a16
Pavel Lokvenc <polygoncz@gmail.com>
2014-07-09 11:56:48 +0200

    Menu and toolbar added.

--- ppm-image-viewer/mainwindow.cpp
+++ ppm-image-viewer/mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->actionOpen->setIcon(this->style()->standardIcon(QStyle::SP_DialogOpenButton));
+    ui->actionReload->setIcon(this->style()->standardIcon(QStyle::SP_BrowserReload));
+    ui->actionExit->setIcon(this->style()->standardIcon(QStyle::SP_DialogCloseButton));
 }
 
 MainWindow::~MainWindow()
